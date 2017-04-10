fb19cc0eef1bf3afd01caba907ef0f4322119346
praveenksarathi <kpraveen15703@gmail.com>
2017-04-10 18:18:11 +0530

    Create main.groovy

---
+++ main.groovy
@@ -0,0 +1,10 @@
+import groovy.json.JsonBuilder
+import groovy.json.JsonSlurper
+
+File f = new File('template.json')
+def slurped = new JsonSlurper().parseText(f.text)
+def builder = new JsonBuilder(slurped)
+
+builder.content.builders[0].image_name = 'Aricent_Image_1'
+
+println(builder.toPrettyString())

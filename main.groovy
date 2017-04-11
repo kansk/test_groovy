b289dacf9706143ba22cee8f438ca1b92ee559e3
praveenksarathi <kpraveen15703@gmail.com>
2017-04-11 10:48:58 +0530

    Update main.groovy

--- main.groovy
+++ main.groovy
@@ -1,6 +1,6 @@
 import groovy.json.JsonBuilder
 import groovy.json.JsonSlurper
-
+def fileName = "packer.json"
 File f = new File('template.json')
 def slurped = new JsonSlurper().parseText(f.text)
 def builder = new JsonBuilder(slurped)
@@ -8,3 +8,13 @@ def builder = new JsonBuilder(slurped)
 builder.content.builders[0].image_name = 'Aricent_Image_1'
 
 println(builder.toPrettyString())
+
+def inputFile = new File(fileName)
+if(inputFile.exists())
+{
+ log.info("A file named " + fileName + " already exisits in the same folder")
+}
+else
+{
+ inputFile.write(builder.toPrettyString())
+}
